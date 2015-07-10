f9346fba9a90acac936af6e9e5a4e318d07930b9
Mohanpattabhi <mohan.pattabhiramaswamy@capgemini.com>
2015-07-10 21:31:35 +0530

    initial commit db scripts

---
+++ UPM_drop.sql
@@ -0,0 +1,17 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2015-07-03 10:39:48.532
+
+
+
+-- tables
+-- Table ADDRESS
+DROP TABLE ADDRESS;
+-- Table OTP
+DROP TABLE USER_OTP;
+-- Table USER
+DROP TABLE USER;
+
+
+
+-- End of file.
+
